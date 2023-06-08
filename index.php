acfdefd9d5052ebcafdd0ffab49f8fa747feb10d
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-06-08 16:08:46 +0200

    add: generate password

--- index.php
+++ index.php
@@ -14,26 +14,38 @@ if (isset($GET['length'])) {
     $uppercase_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $lowercase_chars = 'abcdefghijklmnopqrstuvwxyz';
     $number_chars = '123456789';
-    $symbolchars = '!@#$%^&*()_+~`|}{[]:;?><,./-='
-    
-    
+    $symbol_chars = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+
+    $chars = $uppercase_chars . $lowecase_chars . $number_chars . $symbol_chars;
+
+    $password = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[rand(0, strlen($chars) - 1)];
     }
+} else {
+
+    echo 'Errore lunghezza password non specificata';
+}
+
+
 
 
 
 ?>
 
 <body>
-    <form method="GET" class="password-generator">
+    <div class="password-generator">
         <h2>Generatore di password</h2>
-        <form action="generate_password.php" method="GET">
+        <form action="index.php" method="GET">
             <div class="form-group">
                 <label for="length">Lunghezza della password:</label>
                 <input type="number" id="length" name="length" class="form-control" min="4" max="20" value="8">
             </div>
             <button type="submit" class="btn btn-primary">Genera password</button>
         </form>
-    </form>
+        <div><?php echo $password ?></div>
+    </div>
 </body>
 
 </html>
