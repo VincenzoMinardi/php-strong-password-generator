4f669d84d81215f16c0d037cb21f3c8a18025ba1
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-06-08 15:55:12 +0200

    add symbol,number uppercasechars and lowecasechars

--- index.php
+++ index.php
@@ -1,36 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Generator Password</title>
+    <title>Generatore di password</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 
+
+<?php
+if (isset($GET['length'])) {
+    $length = $GET['length'];
+
+    $uppercase_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $lowercase_chars = 'abcdefghijklmnopqrstuvwxyz';
+    $number_chars = '123456789';
+    $symbolchars = '!@#$%^&*()_+~`|}{[]:;?><,./-='
+    
+    
+    }
+
+
+
+?>
+
 <body>
-    <form class="password-generator">
+    <form method="GET" class="password-generator">
         <h2>Generatore di password</h2>
-        <div class="form-group">
-            <label for="length">Lunghezza della password:</label>
-            <input type="number" id="length" class="form-control" min="4" max="20" value="8">
-        </div>
-        <div class="form-group">
-            <label for="include-uppercase">Includere lettere maiuscole:</label>
-            <input type="text" id="include-uppercase" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="include-numbers">Includere numeri:</label>
-            <input type="text" id="include-numbers" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="include-symbols">Includere simboli:</label>
-            <input type="text" id="include-symbols" class="form-control">
-        </div>
-        <button id="generate" class="btn btn-primary">Genera password</button>
-        <div id="password" class="mt-3"></div>
+        <form action="generate_password.php" method="GET">
+            <div class="form-group">
+                <label for="length">Lunghezza della password:</label>
+                <input type="number" id="length" name="length" class="form-control" min="4" max="20" value="8">
+            </div>
+            <button type="submit" class="btn btn-primary">Genera password</button>
+        </form>
     </form>
 </body>
 
